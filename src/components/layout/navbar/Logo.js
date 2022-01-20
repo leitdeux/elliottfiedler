68b374536021297fb49289899696d2953f7f34b2
leitdeux <elliott.fiedler@gmail.com>
2022-01-20 10:40:53 +0900

    Update theme-ui to fix FOUC issue on load

--- src/components/layout/navbar/Logo.js
+++ src/components/layout/navbar/Logo.js
@@ -1,18 +1,16 @@
 /** @jsxImportSource theme-ui */
 import React from 'react';
 import { Link } from 'gatsby';
-import { Flex } from 'theme-ui';
-
-// import LogoImage from '../../../assets/images/branding/eigopop-logomark.svg';
+import { Heading } from 'theme-ui';
 
 
 export default function Logo() {
 	return (
-		<Link to="/">
-			<Flex sx={{ alignItems: 'center' }}>
-        LOGO GOES HERE
-			</Flex>
-		</Link>
+    <Link href="/" sx={{ color: 'inherit' }}>
+      <Heading as="h3">
+        Elliott Fiedler
+      </Heading>
+    </Link>
 	);
 }
 
